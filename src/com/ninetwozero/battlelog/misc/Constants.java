85bcd54f1d683a616841a5e212fce53924c4a099
Karl Lindmark <karl.lindmark@ninetwozero.com>
2012-02-11 14:27:05 +0100

    OK

--- src/com/ninetwozero/battlelog/misc/Constants.java
+++ src/com/ninetwozero/battlelog/misc/Constants.java
@@ -184,7 +184,7 @@ public final class Constants {
 	public static final String FILE_SHPREF = "battlelog";
 	
 	//Changelog version
-	public final static int CHANGELOG_VERSION = 7;
+	public final static int CHANGELOG_VERSION = 20;
 
 	//MENU IDs
 	public final static int MENU_ID_FEED = 2;
