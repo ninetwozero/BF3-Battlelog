8a1f04452c00c4a012f350aa72e1d2e43558ab7c
Karl Lindmark <karl.lindmark@ninetwozero.com>
2012-11-02 11:28:11 +0100

    Updated the FAQ

--- src/com/ninetwozero/battlelog/activity/aboutapp/AboutFAQFragment.java
+++ src/com/ninetwozero/battlelog/activity/aboutapp/AboutFAQFragment.java
@@ -23,32 +23,19 @@ import com.ninetwozero.battlelog.datatype.DefaultFragment;
 public class AboutFAQFragment extends Fragment implements DefaultFragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // Let's inflate & return the view
-        View view = inflater.inflate(R.layout.tab_content_main_faq,
-                container, false);
-
-        // Let's try this
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_content_main_faq, container, false);
         initFragment(view);
-
-        // Return
         return view;
-
     }
 
     @Override
     public void initFragment(View view) {
-
-        // Prevent bug
         setUserVisibleHint(true);
-
     }
 
     @Override
     public void reload() {
-
     }
 
     @Override
