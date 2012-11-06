33d7d0e33932aac608e4c68ce9a345e038a0c0ae
Karl Lindmark <karl.lindmark@ninetwozero.com>
2012-11-06 02:13:17 +0100

    Stupid eclipse, better start using IntelliJ again before it breaks something.

--- src/com/ninetwozero/battlelog/activity/platoon/PlatoonActivity.java
+++ src/com/ninetwozero/battlelog/activity/platoon/PlatoonActivity.java
@@ -126,13 +126,6 @@ public class PlatoonActivity extends CustomFragmentActivity {
          * Each fragment has its own "cache expiration" date. */
         protected void onPostExecute(Boolean cacheExists) {
             if (cacheExists) {
-            	
-            	/** Reload prevention is not needed at the time of writing.
-            		long cacheExpiration = System.currentTimeMillis()-((Constants.MINUTE_IN_SECONDS*30)*1000);
-                	if(( mPlatoonInformation.getTimestamp() < cacheExpiration)) {
-                		new AsyncRefresh(context, mPlatoonData, SessionKeeper.getProfileData().getId()).execute();
-            		}
-        		*/
             	new AsyncRefresh(context, mPlatoonData, SessionKeeper.getProfileData().getId()).execute();
 
                 mFragmentOverview.show(mPlatoonInformation);
